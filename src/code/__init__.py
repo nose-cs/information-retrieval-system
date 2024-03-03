7ba7f497dca1a4a693feb3460032ef1bd9032856
Anabel Benítez <anabelbenitezglez@gmail.com>
2024-03-03 12:25:48 -0500

    Implement clustering and document recommendation algorithms

--- src/code/__init__.py
+++ src/code/__init__.py
@@ -1,1 +1,4 @@
 from src.code.utils import remove_punctuation, to_lower, tokenize
+from src.code.feedback import RocchioAlgorithm
+from src.code.clustering import ClusterManager
+from src.code.document_recomender import DocumentRecommender
