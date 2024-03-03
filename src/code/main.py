7b7cfbf828748260d4a12cceb42ce37108cc8c53
Anabel Benítez <anabelbenitezglez@gmail.com>
2024-03-03 15:18:23 -0500

    Remove rocchio algorithm cause I couldn't adapt it to extended boolean model

--- src/code/main.py
+++ src/code/main.py
@@ -15,18 +15,20 @@ def terminal_main():
     # print('Boolean Model Built')
 
     extended_boolean_model = ExtendedBooleanModel(corpus)
-    print('Extended Boolean Model Built')
+    print('Extended Boolean Model Built\n')
 
-    query = "beautiful it aushu butterfield are imns great"
+    query = "beautiful it aushu butterfield are great"
 
     # print(f'boolean: {boolean_model.query(query)}')
     # print(f'vector: {vector_model.query(query)}')
     ranked_documents = extended_boolean_model.ranking_function(query)
-    print(f'extended boolean:{ranked_documents}')
+    print(f'extended boolean result:{ranked_documents} \nquery:{query}\n')
     extended_boolean_model.user_feedback(query, [4,2], [doc for doc, rank in ranked_documents])
     extended_boolean_model.pseudo_feedback(query, ranked_documents, 1)
     recommended_documents = extended_boolean_model.get_recommended_documents()
     print(f'recommended documents: {recommended_documents}')
+
+    extended_boolean_model.query(query)
 
 
 def web_main():
