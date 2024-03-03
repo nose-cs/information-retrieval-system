2807c44aec67adbd6ff66e5651cbece3896fb0b6
Anabel Benítez <anabelbenitezglez@gmail.com>
2024-03-03 14:19:03 -0500

    Apparently working simple user's feedback and pseudofeedback for learning user's likes and recommend him/her new documents based on them. Needed to be tested

--- src/code/main.py
+++ src/code/main.py
@@ -8,22 +8,25 @@ def terminal_main():
     corpus = TestCorpus(path=Path('../../data/corpus/'), stemming=True)
     print('Corpus Built')
 
-    vector_model = VectorModel(corpus)
-    print('Vector Model Built')
-
-    boolean_model = BooleanModel(corpus)
-    print('Boolean Model Built')
+    # vector_model = VectorModel(corpus)
+    # print('Vector Model Built')
+    #
+    # boolean_model = BooleanModel(corpus)
+    # print('Boolean Model Built')
 
     extended_boolean_model = ExtendedBooleanModel(corpus)
     print('Extended Boolean Model Built')
 
-    query_1 = "butterflies and (butterflies or not kangaroo)"
-    query_2 = "beautiful it aushu butterfield"
-    query_3 = "not butterflies"
+    query = "beautiful it aushu butterfield are imns great"
 
-    print(f'boolean: {boolean_model.query(query_3)}')
-    print(f'extended boolean: {extended_boolean_model.ranking_function(query_2)}')
-    print(f'vector: {vector_model.query(query_2)}')
+    # print(f'boolean: {boolean_model.query(query)}')
+    # print(f'vector: {vector_model.query(query)}')
+    ranked_documents = extended_boolean_model.ranking_function(query)
+    print(f'extended boolean:{ranked_documents}')
+    extended_boolean_model.user_feedback(query, [4,2], [doc for doc, rank in ranked_documents])
+    extended_boolean_model.pseudo_feedback(query, ranked_documents, 1)
+    recommended_documents = extended_boolean_model.get_recommended_documents()
+    print(f'recommended documents: {recommended_documents}')
 
 
 def web_main():
